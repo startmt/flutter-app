fefa4e0d10c2f8a05320adf293ad73ae437664ae
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-05-18 23:52:49 +0700

    add search page

---
+++ lib/search_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Page 2')
+      );
+  }
+}
