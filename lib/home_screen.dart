c6352db0d47501625ec602a890e6b2592bc2800b
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-05-18 23:52:15 +0700

    add page 1

---
+++ lib/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Page 1')
+      );
+  }
+}
